f964c6fa163d98f9c3c6b7a83da219925615063e
mombeh nadine <nadinemombeh@gmail.com>
2025-11-06 16:21:02 +0100

    solve linters

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import Home from "./pages/Home";
-import Result from "./pages/Result";
 import { Routes, Route, BrowserRouter } from "react-router";
 import store from "./redux/store";
 import { Provider } from "react-redux";
-import EditContact from "./pages/EditContact";
 export default function App() {
 
   return (
@@ -11,8 +9,6 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/add" element={<Result />}></Route>
-          <Route path="/edit/:id" element={<EditContact />}></Route>
         </Routes>
       </BrowserRouter>
     </Provider>
